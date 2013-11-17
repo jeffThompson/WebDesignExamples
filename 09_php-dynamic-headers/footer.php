7889a4f12a344b133e36ad283c999136cf864382
Jeff Thompson <mail@jeffreythompson.org>
2013-11-17 18:08:23 -0500

    Updated PHP examples

--- 09_php-dynamic-headers/footer.php
+++ 09_php-dynamic-headers/footer.php
@@ -1,16 +1,17 @@
-<!-- footer with contact info, etc -->
-<div id="footer">
+		
+		<!-- footer with contact info, etc -->
+		<div id="footer">
 
-	<!-- dynamically create the current year for copyright notice -->
-  <?php
-		/* static start year, current full year is always up to date! */
-		echo"<p>John Doe, all works &copy; 2009–" . date("Y");	
-	?>
-  <span style="float:right"><a href="#">Email</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Facebook</a></span></p>	<!-- a span is an inline element, sort of like a paragraph inside a paragraph -->
-</div>  
-<!-- end footer -->
+			<!-- dynamically create the current year for copyright notice -->
+			<?php
+				/* static start year, current full year is always up to date! */
+				echo"<p>John Doe, all works &copy; 2009–" . date("Y");	
+			?>
+			
+			<span style="float:right"><a href="#">Email</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Facebook</a></span></p>	<!-- a span is an inline element, sort of like a paragraph inside a paragraph -->
+		</div>  <!-- end footer -->
 
-</div>  <!-- end wrapper -->
+		</div>  <!-- end wrapper -->
 
-</body>
+	</body>
 </html>
--- 09_php-dynamic-headers/header.php
+++ 09_php-dynamic-headers/header.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>My First PHP-Based Site</title>
-<link href="stylesheet.css" rel="stylesheet" type="text/css">
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>My First PHP-Based Site</title>
+		<link href="stylesheet.css" rel="stylesheet" type="text/css">
+	</head>
 
-<body>
+	<body>
 
-<!-- overall wrapper, just like previous examples -->
-<div id="wrapper">
+		<!-- overall wrapper, just like previous examples -->
+		<div id="wrapper">
